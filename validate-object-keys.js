0d59b04fbc9da6d7b73424f0b4d7e7d06c1a2d7c
Melody Plan <melodyplan@gmail.com>
2017-03-23 18:49:18 -0700

    validate-object-keys ugghhhhh

---
+++ validate-object-keys.js
@@ -0,0 +1,104 @@
+// running the function with `objectA` and `expectedKeys`
+// should return `true`
+var objectA = {
+  id: 2,
+  name: 'Jane Doe',
+  age: 34,
+  city: 'Chicago'
+}
+
+// running the function with `objectA` and `expectedKeys`
+// should return `false`
+var objectB = {
+  id: 3,
+  age: 33,
+  city: 'Peoria'
+}
+
+var expectedKeys = [
+  'id', 'name', 'age', 'city'
+];
+
+function validateKeys(object, expectedKeys) {  
+ for (Object.validateKeys(expectedKeys).forEach(objectA, objectB){ 
+   return true; 
+ }
+ else  {
+   return false; 
+ }
+
+}
+
+
+/* From here down, you are not expected to 
+   understand.... for now :)  
+   
+   
+   Nothing to see here!
+   
+*/
+
+
+
+function testIt() {
+  var objectA = {
+    id: 2,
+    name: 'Jane Doe',
+    age: 34,
+    city: 'Chicago'
+  }
+
+  var objectB = {
+    id: 3,
+    age: 33,
+    city: 'Peoria'
+  }
+
+  var objectC = {
+    id: 9,
+    name: 'Billy Bear',
+    age: 62,
+    city: 'Milwaukee',
+    status: 'paused'
+  }
+
+  var expectedKeys = [
+    'id', 'name', 'age', 'city'
+  ];
+
+  if (typeof validateKeys(objectA, expectedKeys) !== 'boolean') {
+    console.error(
+      'FAILURE: `validateKeys` should return a boolean value');
+    return;
+  }
+
+
+  if (!validateKeys(objectA, expectedKeys)) {
+    console.error(
+      'FAILURE: running `validateKeys` with the following object and keys ' +
+      'should return `true` but returned `false`:\n' +
+      objectA  + '\n' + expectedKeys
+    )
+    return;
+  }
+
+  if (validateKeys(objectB, expectedKeys)) {
+   console.error(
+      'FAILURE: running `validateKeys` with the following object and keys ' +
+      'should return `false` but returned `true`:\n' +
+      objectB  + '\n' + expectedKeys
+    );
+  }
+
+  if (validateKeys(objectC, expectedKeys)) {
+   console.error(
+      'FAILURE: running `validateKeys` with the following object and keys ' +
+      'should return `false` but returned `true`:\n' +
+      objectC  + '\n' + expectedKeys
+    );
+  }
+  
+  console.log('SUCCESS: `validateKeys` is working');
+}
+
+testIt()
